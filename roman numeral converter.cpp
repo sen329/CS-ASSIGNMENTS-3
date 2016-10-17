e2cae0a177efd5d412876681ba6f41c806a5abd6
sen329 <hans.hilman@gmail.com>
2016-10-17 08:26:36 +0700

    add existing file

---
+++ roman numeral converter.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	
+	cout << "Roman number converter"<<endl;
+	cout << "type a number between 1 - 10: ";
+	cin >> num;
+	
+	switch (num)
+	{
+		case 1: cout << "I" <<endl;
+				break;
+		case 2: cout << "II" <<endl;
+				break;
+		case 3: cout << "III" <<endl;
+				break;
+		case 4: cout << "IV" <<endl;
+				break;
+		case 5: cout << "V" <<endl;
+				break;
+		case 6: cout << "VI" <<endl;
+				break;
+		case 7: cout << "VII" <<endl;
+				break;
+		case 8: cout << "VIII" <<endl;
+				break;
+		case 9: cout << "IX" <<endl;
+				break;
+		case 10: cout << "X"<<endl;
+				break;
+		default: cout << "Between 1 - 10 only"<<endl; 
+	}
+	return 0;
+}
